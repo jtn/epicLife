650c111b1b038ec6bf00c5cdf52becd8da81b59d
jtn <tjelle@gmail.com>
2010-11-13 22:51:11 +0100

    Featstatus can be updated whit a simple click.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,3 +1,12 @@
 $(function() {
-    // Ready when you are, Sir.
+    $('a.feat').click(function(event){
+        var element = $(this);
+        sendValue = element.data('completed') === true ? '0' : '1';
+        $.ajax({
+            url: element.data('id').toString(), 
+            type: 'POST', 
+            data: {_method: 'PUT', "feat[completed]": sendValue },
+            success: function(event){element.toggleClass('completed')}});
+        return false;
+    });// Ready when you are, Sir.
 });
